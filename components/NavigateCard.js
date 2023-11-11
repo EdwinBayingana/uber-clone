8c9ab96eccf672a966e26ca46a0b1efc62133b03
EdwinBayingana <bayinganaedwin@gmail.com>
2023-11-11 10:24:20 +0200

    add the NavigateCardComponent

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,16 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/core';
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
   return (
-    <SafeAreaView style={tw`bg-red-500 flex-1`}>
-      <Text>Yoo</Text>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good morning, Ed</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            styles={toInputBoxStyles}
+            returnKeyType={'search'}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                }),
+              );
+              navigation.navigate('RideOptionsCard');
+            }}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'en',
+            }}
+            debounce={400}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({});
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: '#DDDDDF',
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
