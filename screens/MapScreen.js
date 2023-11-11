d7ace6cefed0b440f36027b58dd0ee0deb3d94c0
EdwinBayingana <bayinganaedwin@gmail.com>
2023-11-11 13:33:18 +0200

    add back navigation functionality on the mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { Icon } from 'react-native-elements';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapView from 'react-native-maps';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation();
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('HomeScreen')}
+        style={tw`bg-gray-50 absolute top-10 left-4 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
