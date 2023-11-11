36d41980e288e7d505ad274008e0cb7370622a79
EdwinBayingana <bayinganaedwin@gmail.com>
2023-11-11 11:51:17 +0200

    finish the implementation of the directions API

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+import { selectDestination, selectOrigin } from '../slices/navSlice';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    // This should zoom and fit to markers, "to and fro"
+    mapRef.current?.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
+
+  // useEffect(() => {
+  //   if (!origin || !destination) return;
+
+  //   const getTravelTime = async () => {
+  //     const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`;
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     dispatch(setTravelTimeInfo(data.rows[0].elements[0]));
+  //   };
+
+  //   getTravelTime();
+  // }, [origin, destination, GOOGLE_MAPS_APIKEY]);
+
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -18,6 +46,17 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+          lineDashPattern={[0]}
+        />
+      )}
+
       {origin?.location && (
         <Marker
           coordinate={{
@@ -27,6 +66,18 @@ const Map = () => {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
