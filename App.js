b8e240eb6ed763d9ef9552cb48141d6fc6e9b63d
EdwinBayingana <bayinganaedwin@gmail.com>
2023-11-11 12:14:40 +0200

    add the KeyboardAvoidingView layout and the navfavourites component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
@@ -15,22 +15,28 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="HomeScreen"
-              component={HomeScreen}
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="MapScreen"
-              component={MapScreen}
-              options={{
-                headerShown: false,
-              }}
-            />
-          </Stack.Navigator>
+          <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
---
+++ components/NavFavourites.js
@@ -0,0 +1,69 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import { Icon } from 'react-native-elements';
+import tw from 'tailwind-react-native-classnames';
+
+const NavFavourites = () => {
+  const favoritesData = [
+    {
+      id: '1336',
+      icon: 'home',
+      name: 'Home',
+      location: { lat: 5.4945, lng: -0.4118 },
+      description: 'Jordan Gospel Centre, Land of Grace',
+    },
+    {
+      id: '3809',
+      icon: 'briefcase',
+      name: 'Work',
+      location: { lat: 5.5497, lng: -0.3522 },
+      description: 'Finger Bites Kitchen, Mile 11',
+    },
+  ];
+
+  //   const dispatch = useDispatch();
+  //   const origin = useSelector(selectOrigin);
+  //   const navigation = useNavigation();
+
+  return (
+    <FlatList
+      data={favoritesData}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => (
+        <View
+          style={[
+            tw`bg-gray-200`,
+            {
+              height: 0.5,
+            },
+          ]}
+        />
+      )}
+      renderItem={({ item: { name, icon, location, description } }) => (
+        <TouchableOpacity style={tw`flex-row items-center py-5`}>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            type="ionicon"
+            color="white"
+            size={18}
+          />
+          <View>
+            <Text style={tw`font-bold text-lg`}>{name}</Text>
+            <Text style={tw`text-gray-500`}>{description}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavFavourites;
+
+const styles = StyleSheet.create({});
